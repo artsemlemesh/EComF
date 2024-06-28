a21c4840bc10aaa400434163986802dc6fd10aa9
artsemlemesh <126878429+artsemlemesh@users.noreply.github.com>
2024-06-28 14:43:48 +0800

    d

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build',
+    outDir: "build",
+  },
+  define: {
+    "process.env": process.env,
   },
   server: {
-    port:3003,
+    port: 3003,
     strictPort: true,
-    host: '0.0.0.0'
+    host: "0.0.0.0",
   },
-})
+});

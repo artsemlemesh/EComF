a149c321368e19ee780de76ccf01748b76036311
artsemlemesh <126878429+artsemlemesh@users.noreply.github.com>
2024-06-28 12:32:25 +0800

    deploy

--- frontend/src/GlobalContext.jsx
+++ frontend/src/GlobalContext.jsx
@@ -40,7 +40,7 @@ export default function GlobalState({ children }) {
     const fetchProducts = async () => {
       try {
         const response = await fetch(
-          `http://127.0.0.1:8000/products/?page=${count}`
+          `https://back-mike-1855d352a13c.herokuapp.com/products/?page=${count}`
         ); //also changed here
         if (!response.ok) {
           throw new Error("Network error was not ok");

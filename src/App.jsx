29e27220d729b6bbc29ab764526718bec7d5e952
artsemlemesh <126878429+artsemlemesh@users.noreply.github.com>
2024-07-23 11:17:32 +0800

    new

--- src/App.jsx
+++ src/App.jsx
@@ -11,14 +11,22 @@ const stripePromise = loadStripe(
   "pk_test_51PLGUYIhhfcmQPRULxV6eXxZHMWqEEW29vQ1FdVwcjE7CevUdT4TnnWVHu15xXC7ZQ7x8CXtXtUbqj3I2zZFlqEn00fS1PbLT0"
 );
 
+const containerStyle = {
+  maxWidth: '1300px',
+  margin: '0 auto',
+  padding: '0 15px',
+}
+
 function App() {
   return (
     <>
       <BrowserRouter>
         <Elements stripe={stripePromise}>
+          <div style={containerStyle}>
           <Nav />
           <MyRoute />
           <Footer />
+          </div>
           <ToastContainer />
         </Elements>
       </BrowserRouter>
